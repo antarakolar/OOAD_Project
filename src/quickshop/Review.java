ac1d7064d1316c9a75ad2ad94dce90cee0cc2b4a
Shriya <shriya.rodi@colorado.edu>
2017-12-10 12:59:46 -0700

    Adding a file

---
+++ src/quickshop/Review.java
@@ -0,0 +1,10 @@
+package Quickshop;
+public interface Review {
+private String review;
+protected String getReviewr() {
+	return review;
+}
+protected void setReview(String review) {
+	this.review = review;
+
+}
