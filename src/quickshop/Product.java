16c2b6d95ab038dbf544b87dfa039cbc4b4d5305
antarakolar <antara.kolar@colorado.edu>
2017-12-10 14:04:17 -0700

    Update

--- src/quickshop/Product.java
+++ src/quickshop/Product.java
@@ -1,5 +1,44 @@
-package quickshop;
+package Quickshop;
 
 public class Product {
+	private int productId;
+	private String productName;
+	private String productCategory;
+	private int productCost; 
+	
+	
+	public int getProductId() {
+		return productId;
+	}
+
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public String getProductCategory() {
+		return productCategory;
+	}
+
+	public void setProductCategory(String productCategory) {
+		this.productCategory = productCategory;
+	}
+
+	public int getProductCost() {
+		return productCost;
+	}
+
+	public void setProductCost(int productCost) {
+		this.productCost = productCost;
+	}
+	
+	
 
 }
