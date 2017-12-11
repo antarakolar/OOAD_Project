a9052392b98f2203703d4bfcc8977d038e321860
Hariramnatarajan <hana5856@colorado.edu>
2017-12-10 23:50:00 -0700

    update

--- src/quickshop/Admin.java
+++ src/quickshop/Admin.java
@@ -3,6 +3,10 @@ package Quickshop;
 public class Admin extends User implements OrderObserver{
 
 	private int AdminID;
+	private String order;
+	private static Admin singleton;
+	private Profile profile; 
+	private User User; 
 
 	protected int getAdminID() {
 		return AdminID;
@@ -11,6 +15,27 @@ public class Admin extends User implements OrderObserver{
 	private void setAdminID(int adminID) {
 		AdminID = adminID;
 	}
+	public void update(Order order);		// Observer design pattern : Observer
 	
+	// Singleton Design Pattern
+	private Admin() {
+	}
+	
+	public static getAddAccount() {
+		return profile; 
+	}
+	public static removeAccount() {
+		return User.userID; 
+	}
+
+	public static Admin getSingleton() {
+	    if (null == singleton) {
+	        singleton = new Admin();
+	    }
+	    return singleton;
+	}
 	
 }
+
+
+
